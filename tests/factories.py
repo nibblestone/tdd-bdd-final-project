d0dfc6ee4f1aa9286235477213265a5d3a1f7b81
Lab Environment <no.one@nowhere.com>
2025-03-03 10:13:13 -0500

    Generate fake products

--- tests/factories.py
+++ tests/factories.py
@@ -31,4 +31,31 @@ class ProductFactory(factory.Factory):
         model = Product
 
     id = factory.Sequence(lambda n: n)
-   ## Add code to create Fake Products 
+    name = FuzzyChoice(
+        choices=[
+            "Hat",
+            "Pants",
+            "Shirt",
+            "Apple",
+            "Banana",
+            "Pots",
+            "Towels",
+            "Ford",
+            "Chevy",
+            "Hammer",
+            "Wrench",
+        ]
+    )
+    description = factory.Faker("text")
+    price = FuzzyDecimal(low=0.5, high=2000.0, precision=2)
+    available = FuzzyChoice(choices=[True, False])
+    category = FuzzyChoice(
+        choices=[
+            Category.UNKNOWN,
+            Category.CLOTHS,
+            Category.FOOD,
+            Category.HOUSEWARES,
+            Category.AUTOMOTIVE,
+            Category.TOOLS,
+        ]
+    )
