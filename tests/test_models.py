055b9ff8c7eccdb2903df4520f8374ec73820599
Lab Environment <no.one@nowhere.com>
2025-03-04 04:38:35 -0500

    Add test for reading a product

--- tests/test_models.py
+++ tests/test_models.py
@@ -101,6 +101,20 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(new_product.available, product.available)
         self.assertEqual(new_product.category, product.category)
 
+    def test_read_a_product(self):
+        """It should Read a product"""
+        product = ProductFactory()
+        product.id = None
+        product.create()
+        self.assertIsNotNone(product.id)
+        found_product = Product.find(product.id)
+        self.assertEqual(found_product.id, product.id)
+        self.assertEqual(found_product.name, product.name)
+        self.assertEqual(found_product.description, product.description)
+        self.assertEqual(found_product.price, product.price)
+        self.assertEqual(found_product.available, product.available)
+        self.assertEqual(found_product.category, product.category)
+
     #
     # ADD YOUR TEST CASES HERE
     #
